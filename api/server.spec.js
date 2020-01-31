713db9bafd301bf3029fea4673a48fd8ae4e82e4
Heather Nuffer <zzzuzz@gmail.com>
2020-01-31 09:50:41 -0700

    Added tests for registration routes

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,3 +6,39 @@ describe('server', function() {
         expect(true).toBe(true);
     });
 });
+
+describe('POST /api/auth/register', function() {
+    it ('should return status 201 after a successful registration', function() {
+        let randomNum = Math.random() * 8000;
+        return request(server)
+        .post('/api/auth/register')
+        .send({"username": `dave${randomNum}`,
+        "password": "toast",
+        })
+        .then(res => {
+            expect(res.status).toBe(201);
+        });
+    });
+    it ('should return JSON after a successful registration', function() {
+        let randomNum = Math.random() * 8000;
+        return request(server)
+        .post('/api/auth/register')
+        .send({"username": `amy${randomNum}`,
+        "password": "toast",
+        })
+        .then(res => {
+            expect(res.type).toMatch(/json/i);
+        });
+    });
+    it ('should return a token after a successful registration', function() {
+        let randomNum = Math.random() * 8000;
+        return request(server)
+        .post('/api/auth/register')
+        .send({"username": `jeff${randomNum}`,
+        "password": "lumber",
+        })
+        .then(res => {
+            expect(res.body.token).toBeDefined();
+        });
+    });      
+});
