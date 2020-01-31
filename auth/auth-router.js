0f2e8c47243f3e98c213a9f21d7e1d5b23cbdcc9
Heather Nuffer <zzzuzz@gmail.com>
2020-01-31 09:20:39 -0700

    Added register route with JSON web token creation

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,38 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  Users.addUser(user)
+      .then(saved => {
+          const token = generateToken(user);
+          res.status(201).json({user: saved, token: token})
+      })
+      .catch(err => {
+          res.status(500).json({error: err, message: 'Failure to add user'})
+      });
 });
 
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+      subject: user.id,
+      username: user.username
+  }
+  const secret = process.env.JWT_SECRET || 'so much to learn to set up a database';
+  const options = {
+      expiresIn: '8h'
+  }
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
---
+++ users/users-model.js
@@ -0,0 +1,20 @@
+module.exports = {
+    addUser,
+    findUserById
+}
+
+const db = require('../database/dbConfig.js');
+
+function addUser(newUser) {
+    return db('users')
+        .insert(newUser)
+        .then(ids => {
+            return findUserById(ids[0]);
+        });
+}
+
+function findUserById(user_id) {
+    return db('users')
+        .where({ id: user_id })
+        .first();
+}
