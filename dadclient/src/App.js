42e8565d533f2ed7c36f70a77b189d0bf5983a22
Heather Nuffer <zzzuzz@gmail.com>
2020-01-31 11:21:38 -0700

    Added front end with register, log in, and view jokes pages

---
+++ dadclient/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { NavLink, Route } from 'react-router-dom';
+import axios from 'axios';
+
+import Signup from './components/Signup';
+import Jokes from './components/Jokes';
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import SignOut from './components/SignOut';
+
+function App() {
+  const [user, setUser] = useState({username: '', password: '', department: ''})
+
+  const registerUser = (user, push) => {
+    axios.post('http://localhost:3300/api/auth/register', user)
+      .then(response => {
+        console.log(response)
+        setUser(response.data);
+        localStorage.setItem('token', response.data.token);
+        push('/jokes');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const loginUser = (user, push) => {
+    console.log('user to submit: ', user);
+    axios.post('http://localhost:3300/api/auth/login', user)
+      .then(response => {
+        console.log(response)
+        setUser(response.data.user);
+        localStorage.setItem('token', response.data.token);
+        push('/jokes');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          Dad Jokes
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <nav>
+         <NavLink to='/home'>Home</NavLink>
+         <NavLink to='/signup'>Sign Up</NavLink>
+         <NavLink to='/signin'>Sign In</NavLink>
+         <NavLink to='/jokes'>Jokes</NavLink>
+         <NavLink to='/signout'>Sign Out</NavLink>
+       </nav>
+      </header>
+
+      <div className='main-content'>
+        <Route exact path='/' component={Home} />
+        <Route path='/signup' render={props => <Signup {...props} registerUser={registerUser} />} />
+        <Route path='/signin' render={props => <SignIn {...props} loginUser={loginUser} />} />
+        <Route path='/jokes' component={Jokes} /> 
+        <Route path='/signout' component={SignOut} />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
