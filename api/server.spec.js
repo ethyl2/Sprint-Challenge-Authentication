3e267adc81bb3ad5ddf728914183950d61357ab9
Heather Nuffer <zzzuzz@gmail.com>
2020-01-31 09:34:05 -0700

    Added testing functionality

---
+++ api/server.spec.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const server = require('./server.js');
+
+describe('server', function() {
+    it('runs the test', function() {
+        expect(true).toBe(true);
+    });
+});
